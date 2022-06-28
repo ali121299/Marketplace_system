83813ab10fe4c9becc75d7e6012bd7675da26fdd
AyaMHassan <54334085+AyaMHassan@users.noreply.github.com>
2022-06-28 02:09:00 +0200

    add the cash and history functions in the server

--- serverApplication.java
+++ serverApplication.java
@@ -12,8 +12,76 @@ public class serverApplication extends Application {
     public void start(Stage stage) throws IOException {
 
     }
+    public float cash(String user_name) {
+    ArrayList<float> temp = new ArrayList<float>();
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/" + DB,"root",DB_password);
+        Statement stmt = con.createStatement();
+        String text= "SELECT Current_balance FROM account,login_signup WHERE account.username =  login_signup.username AND account.username=\""+user_name+"\"";
+        ResultSet rs = stmt.executeQuery(text);
+        while (rs.next()) {
+            temp.add(rs.getString(1));
+        }
+        con.close();
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+    return items[0];
+}
+
+    public ArrayList<String> history(String user_name) {
+        ArrayList<String> temp = new ArrayList<String>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/" + DB,"root",DB_password);
+            Statement stmt = con.createStatement();
+            String text= "SELECT Name,price FROM account,orderspecs,items,orderitems WHERE account.Username =  orderspecs.Client_name " +
+                    "AND orderspecs.OID = orderitems.OID AND account.Username = \""+user_name+"\" AND orderitems.item_name = items.Name " +
+                    ;
+            ResultSet rs = stmt.executeQuery(text);
+            while (rs.next()) {
+                String tempstr = "";
+                for(int i = 1; i < 3; i++) {
+                    tempstr += rs.getString(i) + ":";
+                }
+                temp.add(rs.getString(i));
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return temp;
+    }
+
+    public ArrayList<String> accountInfo(String user_name) {
+        ArrayList<String> temp = new ArrayList<String>();
+        try {
+    //            Class.forName("com.mysql.jdbc.Driver");
+    //            Connection con = DriverManager.getConnection(
+    //                    "jdbc:mysql://localhost:3306/" + DB,"root",DB_password);
+    //            Statement stmt = con.createStatement();
+    //            String text= "SELECT Name,price FROM account,orderspecs,items,orderitems WHERE account.Username =  orderspecs.Client_name " +
+    //                    "AND orderspecs.OID = orderitems.OID AND account.Username = \"" +user_name+"\" AND orderitems.item_name = items.Name " +
+    //                    ;
+    //            ResultSet rs = stmt.executeQuery(text);
+    //            while (rs.next()) {
+    //                String tempstr = "";
+    //                for(int i = 1; i < 3; i++) {
+    //                    tempstr += rs.getString(i) + ":";
+    //                }
+    //                temp.add(rs.getString(i));
+    //            }
+    //            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return temp;
+    }
 
     public static void main(String[] args) {
-        launch();
-    }
+    launch();
+}
 }
