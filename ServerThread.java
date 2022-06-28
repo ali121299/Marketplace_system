01d4c89c69053a4bec8140c08b2c2035e94215e6
ali121299 <85250818+ali121299@users.noreply.github.com>
2022-06-28 15:19:08 +0200

    Add files via upload

---
+++ ServerThread.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+ 
+/**
+ *
+ * @author Lenovo
+ */
+public class ServerThread extends Thread{
+ 
+    Socket socket;
+ 
+ 
+    public ServerThread(Socket socket){
+        this.client = socket;
+    }
+ 
+    @Override
+    public void run(){
+ 
+    }
+}
