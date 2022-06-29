f0c49c82af346bb35cf6c4aa7f068bbe975e0057
Fatma-Badr <1806734@eng.asu.edu.eg>
2022-06-29 20:49:05 +0200

    update sign up screen and icon function

--- clientApplication.java
+++ clientApplication.java
@@ -23,11 +23,15 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class clientApplication extends Application {
-    Image logo=new Image("C:\\Users\\DELL\\Documents\\NetBeansProjects\\market\\src\\market\\market.jpg");
+    
     String Username;
     String Password;
+    String mail;
+    LocalDate birth;
+    int Phone;
     
     @Override
     public void start(Stage stage) throws IOException {
@@ -57,12 +61,12 @@ public class clientApplication extends Application {
         vbox.setMargin(login, new Insets(5, 10, 10, 10));
         vbox.setMargin(signUp, new Insets(10, 10, 10, 10)); 
         Scene scene=new Scene(vbox,400,400);
-        ImageView i=new ImageView(logo);
+        ImageView i=new ImageView(icon());
         i.setFitHeight(scene.getHeight()/3);
         i.setFitWidth(scene.getWidth()/3);
         vbox.setMargin(i, new Insets(10, 10, 10, 10)); 
         vbox.getChildren().addAll(i,login,signUp);
-        stage.getIcons().add(logo);
+        stage.getIcons().add(icon());
         stage.setTitle("Market");
         stage.setScene(scene);
         stage.show();
@@ -131,34 +135,34 @@ public class clientApplication extends Application {
         Stage signUp=new Stage();
         VBox v=new VBox();
         v.setAlignment(Pos.CENTER);
-        Scene s=new Scene(v,300,300);
+        Scene s=new Scene(v,400,400);
         signUp.setScene(s);
         signUp.setTitle("Sign up screen");
-        signUp.getIcons().add(logo);
+        signUp.getIcons().add(icon());
         HBox h1=new HBox(7);
         h1.setAlignment(Pos.CENTER);
-        Label email=new Label("     Email    ");
+        Label email=new Label("     Email     ");
         h1.setMargin(email,new Insets(10, 10, 10, 10));
         TextField Email=new TextField();
         h1.setMargin(Email,new Insets(10, 10, 10, 10));
         h1.getChildren().addAll(email,Email);
         HBox h2=new HBox(7);
         h2.setAlignment(Pos.CENTER);
-        Label username=new Label("Username");
+        Label username=new Label("Username ");
         h2.setMargin(username,new Insets(10, 10, 10, 10));
         TextField user=new TextField();
         h2.setMargin(user,new Insets(10, 10, 10, 10));
         h2.getChildren().addAll(username,user);
         HBox h3=new HBox(7);
         h3.setAlignment(Pos.CENTER);
-        Label pass=new Label("Password");
+        Label pass=new Label("Password  ");
         h3.setMargin(pass,new Insets(10, 10, 10, 10));
         TextField password=new TextField();
         h3.setMargin(password,new Insets(10, 10, 10, 10));
         h3.getChildren().addAll(pass,password);
         HBox h4=new HBox(7);
         h4.setAlignment(Pos.CENTER);
-        Label birthday=new Label("Birthday");
+        Label birthday=new Label("Birthday   ");
         h4.setMargin(birthday,new Insets(10, 10, 10, 10));
         TextField Birthday=new TextField();
         h4.setMargin(Birthday,new Insets(10, 10, 10, 10));
@@ -171,6 +175,21 @@ public class clientApplication extends Application {
         h5.setMargin(phone,new Insets(10, 10, 10, 10));
         h5.getChildren().addAll(telephone,phone);
         Button sign=new Button("Sign Up");
+        sign.setOnMouseClicked((new EventHandler <MouseEvent>(){
+            @Override
+            public void handle(MouseEvent t) {
+                Username=user.getText();
+                Password=password.getText();
+                birth=LocalDate.parse(Birthday.getText());
+                mail =Email.getText();
+                Phone=Integer.parseInt(phone.getText());
+                String s=Username+","+Password+","+mail+","+String.valueOf(birth)+","+String.valueOf(Phone);
+                //signUp_handler(s);
+//                request1("signup",s);
+                signUp.close();
+                //first_page();
+            }            
+        }));
         v.getChildren().addAll(h1,h2,h3,h4,h5,sign);
         signUp.showAndWait();
     }
